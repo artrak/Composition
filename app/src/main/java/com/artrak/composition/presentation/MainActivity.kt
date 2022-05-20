20f111284c7eededceb099a8982296c44069e992
Artem <artrak9@gmail.com.ua>
2022-05-20 13:22:44 +0300

    finish lesson 1 and 2

--- app/src/main/java/com/artrak/composition/presentation/MainActivity.kt
+++ app/src/main/java/com/artrak/composition/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.artrak.composition
+package com.artrak.composition.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.artrak.composition.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
